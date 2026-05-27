e4d63e222408007c0f48c0534ff7e74a40d3d293
theamadison <alfoxthea@gmail.com>
2026-05-27 00:05:51 -0400

    Add files via upload

--- src/shows/ShowSelection.jsx
+++ src/shows/ShowSelection.jsx
@@ -1,6 +1,22 @@
 import "./shows.css";
 
-/** A navbar that allows users to choose between a list of shows */
-export default function ShowSelection() {
-  return <nav className="shows"></nav>;
+/** A navbar that lets the viewers choose between a list of different shows */
+export default function ShowSelection({
+  shows,
+  selectedShow,
+  setSelectedShow,
+}) {
+  return (
+    <nav className="shows">
+      {shows.map((show) => (
+        <a
+          className={"show" + (show === selectedShow ? " selected" : "")}
+          key={show.name}
+          onClick={() => setSelectedShow(show)}
+        >
+          {show.name}
+        </a>
+      ))}
+    </nav>
+  );
 }
