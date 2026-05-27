86ffdda7b643b73b73468c4f2cb328fa49cbf7b0
theamadison <alfoxthea@gmail.com>
2026-05-27 00:06:26 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-/**
- * React TV is an web streaming platform that allows users to browse
- * through the episodes of a variety of different shows.
- */
+import { useState } from "react";
+import { tvShows } from "./shows/data";
+import ShowDetails from "./shows/ShowDetails";
+import ShowSelection from "./shows/ShowSelection";
+
+/** A web streaming platform named React TV.
+ * This platform allows viewers to browse through a selection of different shows and their episodes. */
 export default function App() {
+  const [shows] = useState(tvShows);
+  const [selectedShow, setSelectedShow] = useState();
+
   return (
     <>
       <header>
         <p>React TV</p>
+        <ShowSelection
+          shows={shows}
+          selectedShow={selectedShow}
+          setSelectedShow={setSelectedShow}
+        />
       </header>
-      <main></main>
+
+      {/*
+        Changing the `key` prop will force the ShowDetails to rerender
+        whenever the selected show changes.
+      */}
+      <main>
+        <ShowDetails key={selectedShow?.name} show={selectedShow} />
+      </main>
     </>
   );
 }
